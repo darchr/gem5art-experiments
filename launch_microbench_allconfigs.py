c60812e5cc82fca70c04f232e35c04cef72763a7
krithiga <kmurugavel@ucdavis.edu>
2020-01-21 15:28:28 -0800

    adding command line param

--- launch_microbench_allconfigs.py
+++ launch_microbench_allconfigs.py
@@ -4,11 +4,13 @@
 
 import os
 import sys
+import argparse
 from uuid import UUID
 
 from gem5art.artifact.artifact import Artifact
 from gem5art.run import gem5Run
 from gem5art import tasks
+
 
 
 experiments_repo = Artifact.registerArtifact(
@@ -50,10 +52,8 @@ gem5_binary = Artifact.registerArtifact(
     documentation = 'default gem5 x86'
 )
 
+
 if __name__ == "__main__":
-    cpu_types = ['Simple','DefaultO3'] 
-    mem_latency = ['Inf','SingleCycle'] #['Slow', 'Inf' 'SingleCycle']
-    config='config1' #'config2''config3'
 
     full_list =['CCa','CCe','CCh', 'CCh_st', 'CCl','CCm','CF1','CRd','CRf','CRm',
     'CS1','CS3','DP1d','DP1f','DPcvt','DPT','DPTd','ED1','EF','EI','EM1','EM5',
@@ -63,16 +63,29 @@ if __name__ == "__main__":
     'CS1','CS3']
     memorybenchmarks = ['MD','MC','MCS','M_Dyn','MI','MIM','MIM2','MIP','ML2','ML2_BW_ld','ML2_BW_ldst',
     'ML2_BW_st','ML2_st','MM','MM_st','STc','STL2','STL2b']
-    main_Memorybenchmarks = ['MM','MM_st','STc','STL2','STL2b']
-    bm_list =  full_list 
+    main_memorybenchmarks = ['MM','MM_st','STc','STL2','STL2b']
+    cpu_types = ['Simple','DefaultO3'] 
+    mem_latency = ['Inf','SingleCycle','Slow'] #['Slow', 'Inf' 'SingleCycle']
 
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', choices = ['config_base','config_controlbenchmarks','config_memorybenchmarks']
+                                               ,default='config_base',help = "experiment you want to run")
+    parser.add_argument('--bm_list',choices =[full_list,controlbenchmarks,memorybenchmarks,main_memorybenchmarks]
+                                            ,default=full_list, help = "benchmark suite to run the experiment")
+    parser.add_argument('--cpu', choices = ['Simple','O3'], default='Simple',help="cpu choice for controlbenchmark experiments.")
+    parser.add_argument('--cache_type', type = str, choices = ['L1_cache','L2_cache'],
+                                            default='L1_cache',help = "cache type for memory experiment")
+    args  = parser.parse_args()
 
-    #cpus for branchpredictors
-    cpu_bp='Simple' #O3
+    config = agrs.config
+    bm_list =  args.bm_list
+    
+    cpu_bp= args.cpu
     Simple_bp=('Simple_Local', 'Simple_BiMode', 'Simple_Tournament', 'Simple_LTAGE')
     DefaultO3_bp=('DefaultO3_Local' ,'DefaultO3_BiMode', 'DefaultO3_Tournament','DefaultO3_LTAGE')
     
-    cache_type = 'L1_cache' #'L2_cache'
+    cache_type = args.cache_type #'L2_cache'
     #L1Cache_sizes.
     L1D = ['4kB','32kB','64kB']
     #L2Cache_sizes.
@@ -87,7 +100,7 @@ if __name__ == "__main__":
 
     path = 'microbench'
     
-    if config == 'config1':
+    if config == 'config_base':
       for mem in mem_latency:
         for bm in bm_list:
             for cpu in cpu_types:
@@ -99,7 +112,7 @@ if __name__ == "__main__":
                         cpu, mem, os.path.join(path,bm,bma))
                     #run_gem5_instance.apply_async((run,))
                     run.run() 
-    elif config == 'config2':
+    elif config == 'config_controlbenchmarks':
         for mem in mem_latency:
             for bm in bm_list:
                 if cpu_bp == 'Simple':
@@ -122,7 +135,7 @@ if __name__ == "__main__":
                                 config_cpu, mem, os.path.join(path,bm,bma))
                         #run_gem5_instance.apply_async((run,))
                         run.run()
-    elif config =='config3':
+    elif config =='config_memorybenchmarks':
         for mem in mem_latency:
             for bm  in bm_list:
                 for cpu in cpu_types:
