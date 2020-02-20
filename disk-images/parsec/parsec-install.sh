972d3627ef99bb77c94265fe8dd88613e35e0508
mahyarsamani <msamani@ucdavis.edu>
2020-02-19 17:26:12 -0800

    Initial commmit.

---
+++ disk-images/parsec/parsec-install.sh
@@ -0,0 +1,33 @@
+cd /home/gem5/
+echo "12345" | sudo apt update
+
+# Allowing services to restart while updating some 
+# libraries.
+sudo apt install -y debconf-utils
+sudo debconf-get-selections | grep restart-without-asking > libs.txt
+sed -i 's/false/true/g' libs.txt
+while read line; do echo $line | sudo debconf-set-selections; done < libs.txt
+sudo rm libs.txt
+##
+
+# Installing packages needed to build PARSEC
+sudo apt install -y build-essential 
+sudo apt install -y m4 
+sudo apt install -y git  
+sudo apt install -y python
+sudo apt install -y python-dev
+sudo apt install -y gettext
+sudo apt install -y libx11-dev 
+sudo apt install -y libxext-dev 
+sudo apt install -y xorg-dev 
+sudo apt install -y unzip
+sudo apt install -y texinfo
+sudo apt install -y freeglut3-dev
+## 
+
+# Building PARSEC
+unzip parsec-benchmark.zip 
+cd parsec-benchmark
+./install.sh
+./get-inputs
+##
