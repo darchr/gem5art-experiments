a641eef82c30541529a0a9fee3b02841c3340aec
Ayaz Akram <yazakram@ucdavis.edu>
2020-02-25 11:24:24 -0800

    Add a script to generate working boot experiments info

---
+++ others/boot-experiments/boot_status.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+from uuid import UUID
+import json
+
+db = MongoClient().artifact_database
+
+
+'''
+This script reads the gem5 run objects
+created by boot tests and creates a list
+of runs that were successful
+
+Output list consists of strings of the format:
+[cpu-model]_[linux-version]_[boot-type]_[mem-model]_[cpu-count]
+
+For now this script is reading run objects from
+the stored files on the file system.
+Later, it will be updated to get this information from
+the database
+
+Anyone interested in finding out the status of linux
+boot tests for their runs, should be able to
+tokenize the elements of the list to get the
+working configuration (cpu model, linux version etc.)
+'''
+
+linuxes = ['5.2.3', '4.19.83', '4.14.134', '4.9.186', '4.4.186']
+boot_types = ['init', 'systemd']
+cpu_types = ['kvm', 'atomic', 'simple', 'o3']
+num_cpus = ['1', '2', '4', '8']
+mem_types = ['classic', 'ruby']
+
+name_exists = 0
+
+
+working_list = []
+
+for linux in linuxes:
+    for boot_type in boot_types:
+        for cpu in cpu_types:
+            for num_cpu in num_cpus:
+                for mem in mem_types:
+
+                    with open('/fasthome/aakahlow/boot_tests/results/run_exit/vmlinux-{}/boot-exit/{}/{}/{}/{}/info.json'.format(linux, cpu, mem, num_cpu, boot_type)) as f:
+                        data = json.load(f)
+                        if(data['status'] == 'Finished'):
+                            working_list.append('{}_{}_{}_{}_{}'.format(cpu,linux,boot_type,mem,num_cpu))
+
+
+
