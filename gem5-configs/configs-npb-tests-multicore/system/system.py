28896d41956688a9b313a830d388db3a5d6a2f98
Ayaz Akram <yazakram@ucdavis.edu>
2020-05-10 19:52:29 -0700

    Update npb tests gem5 configs for gem5-20

--- gem5-configs/configs-npb-tests-multicore/system/system.py
+++ gem5-configs/configs-npb-tests-multicore/system/system.py
@@ -34,7 +34,7 @@ from fs_tools import *
 from caches import *
 
 
-class MySystem(LinuxX86System):
+class MySystem(System):
 
     SimpleOpts.add_option("--no_host_parallel", default=False,
                 action="store_true",
@@ -85,12 +85,12 @@ class MySystem(LinuxX86System):
             self.setDiskImages(disk, disk)
 
         # Change this path to point to the kernel you want to use
-        self.kernel = kernel
+        self.workload.object_file = kernel
         # Options specified on the kernel command line
         boot_options = ['earlyprintk=ttyS0', 'console=ttyS0', 'lpj=7999923',
                          'root=/dev/hda1']
 
-        self.boot_osflags = ' '.join(boot_options)
+        self.workload.command_line = ' '.join(boot_options)
 
         # Create the CPUs for our system.
         self.createCPU(num_cpus)
@@ -247,6 +247,7 @@ class MySystem(LinuxX86System):
 
     def initFS(self, membus, cpus):
         self.pc = Pc()
+        self.workload = X86FsLinux()
 
         # Constants similar to x86_traits.hh
         IO_address_space_base = 0x8000000000000000
@@ -306,7 +307,7 @@ class MySystem(LinuxX86System):
         ###############################################
 
         # Add in a Bios information structure.
-        self.smbios_table.structures = [X86SMBiosBiosInformation()]
+        self.workload.smbios_table.structures = [X86SMBiosBiosInformation()]
 
         # Set up the Intel MP table
         base_entries = []
@@ -364,8 +365,8 @@ class MySystem(LinuxX86System):
         assignISAInt(1, 1)
         for i in range(3, 15):
             assignISAInt(i, i)
-        self.intel_mp_table.base_entries = base_entries
-        self.intel_mp_table.ext_entries = ext_entries
+        self.workload.intel_mp_table.base_entries = base_entries
+        self.workload.intel_mp_table.ext_entries = ext_entries
 
         entries = \
            [
@@ -393,5 +394,5 @@ class MySystem(LinuxX86System):
             size='%dB' % (self.mem_ranges[-1].size()),
             range_type=1))
 
-        self.e820_table.entries = entries
+        self.workload.e820_table.entries = entries
 
